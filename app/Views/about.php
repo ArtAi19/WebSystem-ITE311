c8e6ebf2e42ff2e59c6dbdb3c8e3c39aacc6c0aa
ArtChoy <getis985@gmail.com>
2025-09-19 01:11:49 +0800

    Added Home controller, routes, and views for basic navigation

---
+++ app/Views/about.php
@@ -0,0 +1,11 @@
+<?= $this->extend('template') ?>
+
+<?= $this->section('content') ?>
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="text-center mb-5">
+                <h1 class="display-4">About Us</h1>
+                <p class="lead">Empowering learners through technology</p>
+            </div>
+<?= $this->endSection() ?>
